b09cb4958ecff47475ff3e9c78f2a9805266d0fc
mg152 <mg152git@gmail.com>
2022-07-29 08:57:16 +0300

    Move grapheme ranges to the proper place

--- src/cpp_uni_break_grapheme.h
+++ src/cpp_uni_break_grapheme.h
@@ -14,8 +14,285 @@
 
 #include "cpp_uni_config.h"
 #include "cpp_uni_version.h"
+#include "internal/cpp_uni_ranges_core.h"
 
 #include "impl/impl_break_grapheme.h"
+
+namespace uni {
+
+namespace ranges::grapheme {
+
+template<class Range>
+class utf8_view : public detail::ranges::view_base
+{
+private:
+    template<class Iter, class Sent>
+    class utf8
+    {
+        static_assert(detail::ranges::sa_iter_contiguous<Iter>::value &&
+                      std::is_integral_v<detail::ranges::iter_value_t<Iter>>,
+                      "grapheme::utf8 view requires contiguous UTF-8 range");
+
+    private:
+        utf8_view* parent = nullptr;
+        Iter it_begin;
+        Iter it_pos;
+        Iter it_next;
+
+        detail::impl_break_grapheme_state state{};
+
+        void iter_func_grapheme_utf8()
+        {
+            it_begin = it_pos;
+
+            while (it_next != std::end(parent->range))
+            {
+                it_pos = it_next;
+                uni::detail::type_codept codepoint = 0;
+                it_next = detail::inline_utf8_iter(it_next, std::end(parent->range), &codepoint, detail::impl_iter_replacement);
+                if (detail::inline_break_grapheme(&state, codepoint))
+                    return;
+            }
+
+            if (it_next == std::end(parent->range))
+                it_pos = it_next;
+        }
+
+    public:
+        using iterator_category = std::forward_iterator_tag;
+        using value_type        = std::basic_string_view<detail::ranges::iter_value_t<Iter>>;
+        using pointer           = void;
+        using reference         = value_type;
+        using difference_type   = detail::ranges::iter_difference_t<Iter>;
+
+        utf8() = default;
+        explicit utf8(utf8_view& p, Iter begin, Sent end)
+            : parent{&p}, it_begin{begin}, it_pos{begin}, it_next{begin}
+        {
+            if (begin == end)
+                return;
+
+            detail::impl_break_grapheme_state_reset(&state);
+
+            iter_func_grapheme_utf8();
+        }
+        uaiw_constexpr reference operator*() const
+        {
+            return detail::ranges::to_string_view<reference>(parent->range, it_begin, it_pos);
+        }
+        uaiw_constexpr Iter begin() const noexcept { return it_begin; }
+        uaiw_constexpr Iter end() const noexcept { return it_pos; }
+        utf8& operator++()
+        {
+            iter_func_grapheme_utf8();
+
+            return *this;
+        }
+        utf8 operator++(int)
+        {
+            utf8 tmp = *this;
+            operator++();
+            return tmp;
+        }
+        friend bool operator==(const utf8& x, const utf8& y) { return (x.it_begin == y.it_begin); }
+        friend bool operator!=(const utf8& x, const utf8& y) { return (x.it_begin != y.it_begin); }
+    private:
+        static uaiw_constexpr bool friend_compare_sentinel(const utf8& x) { return x.it_begin == std::end(x.parent->range); }
+    public:
+        friend uaiw_constexpr bool operator==(const utf8& x, uni::sentinel_t) { return friend_compare_sentinel(x); }
+        friend uaiw_constexpr bool operator!=(const utf8& x, uni::sentinel_t) { return !friend_compare_sentinel(x); }
+        friend uaiw_constexpr bool operator==(uni::sentinel_t, const utf8& x) { return friend_compare_sentinel(x); }
+        friend uaiw_constexpr bool operator!=(uni::sentinel_t, const utf8& x) { return !friend_compare_sentinel(x); }
+    };
+
+    using iter_t = detail::ranges::iterator_t<Range>;
+    using sent_t = detail::ranges::sentinel_t<Range>;
+
+    Range range = Range{};
+    utf8<iter_t, sent_t> cached_begin_value;
+    bool cached_begin = false;
+
+public:
+    uaiw_constexpr utf8_view() = default;
+    uaiw_constexpr utf8_view(Range r) : range{std::move(r)} {}
+    //uaiw_constexpr Range base() const & { return range; }
+    //uaiw_constexpr Range base() && { return std::move(range); }
+    uaiw_constexpr auto begin()
+    {
+        if (cached_begin)
+            return cached_begin_value;
+
+        cached_begin_value = utf8<iter_t, sent_t>{*this, std::begin(range), std::end(range)};
+        cached_begin = true;
+
+        return cached_begin_value;
+    }
+    uaiw_constexpr auto end()
+    {
+        return utf8<iter_t, sent_t>{*this, std::end(range), std::end(range)};
+    }
+    //uaiw_constexpr bool empty() { return begin() == end(); }
+    //explicit uaiw_constexpr operator bool() { return !empty(); }
+};
+
+template<class Range>
+class utf16_view : public detail::ranges::view_base
+{
+private:
+    template<class Iter, class Sent>
+    class utf16
+    {
+        static_assert(detail::ranges::sa_iter_contiguous<Iter>::value &&
+                      std::is_integral_v<detail::ranges::iter_value_t<Iter>> &&
+                      sizeof(detail::ranges::iter_value_t<Iter>) >= sizeof(char16_t),
+                      "grapheme::utf16 view requires contiguous UTF-16 range");
+
+    private:
+        utf16_view* parent = nullptr;
+        Iter it_begin;
+        Iter it_pos;
+        Iter it_next;
+
+        detail::impl_break_grapheme_state state{};
+
+        void iter_func_grapheme_utf16()
+        {
+            it_begin = it_pos;
+
+            while (it_next != std::end(parent->range))
+            {
+                it_pos = it_next;
+                uni::detail::type_codept codepoint = 0;
+                it_next = detail::inline_utf16_iter(it_next, std::end(parent->range), &codepoint, detail::impl_iter_replacement);
+                if (detail::inline_break_grapheme(&state, codepoint))
+                    return;
+            }
+
+            if (it_next == std::end(parent->range))
+                it_pos = it_next;
+        }
+
+    public:
+        using iterator_category = std::forward_iterator_tag;
+        using value_type        = std::basic_string_view<detail::ranges::iter_value_t<Iter>>;
+        using pointer           = void;
+        using reference         = value_type;
+        using difference_type   = detail::ranges::iter_difference_t<Iter>;
+
+        utf16() = default;
+        explicit utf16(utf16_view& p, Iter begin, Sent end)
+            : parent{&p}, it_begin{begin}, it_pos{begin}, it_next{begin}
+        {
+            if (begin == end)
+                return;
+
+            detail::impl_break_grapheme_state_reset(&state);
+
+            iter_func_grapheme_utf16();
+        }
+        uaiw_constexpr reference operator*() const
+        {
+            return detail::ranges::to_string_view<reference>(parent->range, it_begin, it_pos);
+        }
+        uaiw_constexpr Iter begin() const noexcept { return it_begin; }
+        uaiw_constexpr Iter end() const noexcept { return it_pos; }
+        utf16& operator++()
+        {
+            iter_func_grapheme_utf16();
+
+            return *this;
+        }
+        utf16 operator++(int)
+        {
+            utf16 tmp = *this;
+            operator++();
+            return tmp;
+        }
+        friend bool operator==(const utf16& x, const utf16& y) { return (x.it_begin == y.it_begin); }
+        friend bool operator!=(const utf16& x, const utf16& y) { return (x.it_begin != y.it_begin); }
+    private:
+        static uaiw_constexpr bool friend_compare_sentinel(const utf16& x) { return x.it_begin == std::end(x.parent->range); }
+    public:
+        friend uaiw_constexpr bool operator==(const utf16& x, uni::sentinel_t) { return friend_compare_sentinel(x); }
+        friend uaiw_constexpr bool operator!=(const utf16& x, uni::sentinel_t) { return !friend_compare_sentinel(x); }
+        friend uaiw_constexpr bool operator==(uni::sentinel_t, const utf16& x) { return friend_compare_sentinel(x); }
+        friend uaiw_constexpr bool operator!=(uni::sentinel_t, const utf16& x) { return !friend_compare_sentinel(x); }
+    };
+
+    using iter_t = detail::ranges::iterator_t<Range>;
+    using sent_t = detail::ranges::sentinel_t<Range>;
+
+    Range range = Range{};
+    utf16<iter_t, sent_t> cached_begin_value;
+    bool cached_begin = false;
+
+public:
+    uaiw_constexpr utf16_view() = default;
+    uaiw_constexpr utf16_view(Range r) : range{std::move(r)} {}
+    //uaiw_constexpr Range base() const & { return range; }
+    //uaiw_constexpr Range base() && { return std::move(range); }
+    uaiw_constexpr auto begin()
+    {
+        if (cached_begin)
+            return cached_begin_value;
+
+        cached_begin_value = utf16<iter_t, sent_t>{*this, std::begin(range), std::end(range)};
+        cached_begin = true;
+
+        return cached_begin_value;
+    }
+    uaiw_constexpr auto end()
+    {
+        return utf16<iter_t, sent_t>{*this, std::end(range), std::end(range)};
+    }
+    //uaiw_constexpr bool empty() { return begin() == end(); }
+    //explicit uaiw_constexpr operator bool() { return !empty(); }
+};
+
+template<class Range>
+utf8_view(Range&&) -> utf8_view<uni::views::all_t<Range>>;
+template<class Range>
+utf16_view(Range&&) -> utf16_view<uni::views::all_t<Range>>;
+
+} // namespace ranges::grapheme
+
+namespace detail {
+
+/* GRAPHEME_UTF8_VIEW */
+
+struct adaptor_grapheme_utf8
+{
+    template<class R>
+    uaiw_constexpr auto operator()(R&& r) const { return uni::ranges::grapheme::utf8_view{std::forward<R>(r)}; }
+};
+template<class R>
+uaiw_constexpr auto operator|(R&& r, const adaptor_grapheme_utf8& a) { return a(std::forward<R>(r)); }
+
+/* GRAPHEME_UTF16_VIEW */
+
+struct adaptor_grapheme_utf16
+{
+    template<class R>
+    uaiw_constexpr auto operator()(R&& r) const { return uni::ranges::grapheme::utf16_view{std::forward<R>(r)}; }
+};
+template<class R>
+uaiw_constexpr auto operator|(R&& r, const adaptor_grapheme_utf16& a) { return a(std::forward<R>(r)); }
+
+} // namespace detail
+
+namespace ranges::views::grapheme {
+
+inline constexpr detail::adaptor_grapheme_utf8 utf8;
+inline constexpr detail::adaptor_grapheme_utf16 utf16;
+
+}
+
+namespace views = ranges::views;
+
+} // namespace uni
+
+// ITERATORS
+// TODO: Clean
 
 namespace uni::breaks::grapheme {
 
