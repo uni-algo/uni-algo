ece0a70d2077014ebeee5a9c90315112693e827e
mg152 <mg152git@gmail.com>
2022-12-01 07:09:18 +0300

    Regenerate Unicode data files

--- include/uni_algo/impl/data/extern_break_grapheme.h
+++ include/uni_algo/impl/data/extern_break_grapheme.h
@@ -2,7 +2,7 @@
 
 UNI_ALGO_IMPL_NAMESPACE_BEGIN
 
-UNI_ALGO_DLL extern const unsigned char stage1_break_grapheme[8704];
-UNI_ALGO_DLL extern const unsigned char stage2_break_grapheme[19456];
+UNI_ALGO_DLL extern const uaix_data_array(unsigned char, stage1_break_grapheme, 8704);
+UNI_ALGO_DLL extern const uaix_data_array(unsigned char, stage2_break_grapheme, 19456);
 
 UNI_ALGO_IMPL_NAMESPACE_END
--- include/uni_algo/impl/data/extern_break_word.h
+++ include/uni_algo/impl/data/extern_break_word.h
@@ -2,7 +2,7 @@
 
 UNI_ALGO_IMPL_NAMESPACE_BEGIN
 
-UNI_ALGO_DLL extern const unsigned char stage1_break_word[8704];
-UNI_ALGO_DLL extern const unsigned char stage2_break_word[29056];
+UNI_ALGO_DLL extern const uaix_data_array(unsigned char, stage1_break_word, 8704);
+UNI_ALGO_DLL extern const uaix_data_array(unsigned char, stage2_break_word, 29056);
 
 UNI_ALGO_IMPL_NAMESPACE_END
