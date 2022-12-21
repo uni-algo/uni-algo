11ac0c8ac61086dc0038d1800df034fefeff99fa
mg152 <mg152git@gmail.com>
2022-12-21 15:09:32 +0300

    Make search class constexpr

--- include/uni_algo/internal/search.h
+++ include/uni_algo/internal/search.h
@@ -15,13 +15,13 @@ namespace uni {
 class search
 {
 public:
-    search() noexcept = default;
-    explicit search(bool f, std::size_t p, std::size_t e) noexcept
+    constexpr search() noexcept = default;
+    constexpr explicit search(bool f, std::size_t p, std::size_t e) noexcept
         : found(f), position(p), end_position(e) {}
-    explicit operator bool() const noexcept { return found; }
-    void reset() { found = false; position = detail::impl_npos; end_position = detail::impl_npos; }
-    std::size_t pos() const noexcept { assert(found); return position; }
-    std::size_t end_pos() const noexcept { assert(found); return end_position; }
+    constexpr explicit operator bool() const noexcept { return found; }
+    constexpr void reset() { found = false; position = detail::impl_npos; end_position = detail::impl_npos; }
+    constexpr std::size_t pos() const noexcept { assert(found); return position; }
+    constexpr std::size_t end_pos() const noexcept { assert(found); return end_position; }
 private:
     bool found = false;
     std::size_t position = detail::impl_npos;
