db190a3d075061bf3d87e77049c28aa575328af9
mg152 <mg152git@gmail.com>
2023-03-01 19:05:20 +0300

    Regenerate Unicode data files

---
+++ include/uni_algo/impl/data/extern_script.h
@@ -0,0 +1,9 @@
+// Unicode 15.0.0
+
+UNI_ALGO_IMPL_NAMESPACE_BEGIN
+
+UNI_ALGO_DLL extern const uaix_data_array(unsigned char, stage1_script, 8704);
+UNI_ALGO_DLL extern const uaix_data_array(unsigned char, stage2_script, 31744);
+UNI_ALGO_DLL extern const uaix_data_array(type_codept,   stage3_script, 164);
+
+UNI_ALGO_IMPL_NAMESPACE_END
