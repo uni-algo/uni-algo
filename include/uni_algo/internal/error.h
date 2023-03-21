cc2e715e5f23e8e6b2155dd77aacd8c1e2a2d542
mg152 <mg152git@gmail.com>
2023-03-21 19:01:22 +0300

    Add missing explicit to error class

--- include/uni_algo/internal/error.h
+++ include/uni_algo/internal/error.h
@@ -20,8 +20,8 @@ public:
     enum class code : unsigned short {success = 0, ill_formed_utf};
 public:
     constexpr error() noexcept = default;
-    constexpr error(error::code ec) noexcept : error_code{ec} {}
-    constexpr error(error::code ec, std::size_t p) noexcept : position{p}, error_code{ec} {}
+    constexpr explicit error(error::code ec) noexcept : error_code{ec} {}
+    constexpr explicit error(error::code ec, std::size_t p) noexcept : position{p}, error_code{ec} {}
     constexpr explicit operator bool() const noexcept { return error_code != error::code::success; }
     constexpr void reset() noexcept { error_code = error::code::success; position = detail::impl_npos; }
     constexpr bool has_pos() const noexcept { return position != detail::impl_npos; }
