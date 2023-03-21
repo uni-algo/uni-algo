e3c382c7059701d940be92f5e256b6df0056d7f2
mg152 <mg152git@gmail.com>
2023-03-21 19:05:18 +0300

    Refactor search class

--- include/uni_algo/internal/search.h
+++ include/uni_algo/internal/search.h
@@ -13,16 +13,16 @@ namespace una {
 
 class search
 {
+private:
+    std::size_t found_pos = detail::impl_npos;
+    std::size_t found_end_pos = detail::impl_npos;
 public:
     constexpr search() noexcept = default;
-    constexpr explicit search(std::size_t p, std::size_t e) noexcept : position{p}, end_position{e} {}
-    constexpr explicit operator bool() const noexcept { return position != detail::impl_npos; }
-    constexpr void reset() noexcept { position = detail::impl_npos; end_position = detail::impl_npos; }
-    constexpr std::size_t pos() const noexcept { assert(operator bool()); return position; }
-    constexpr std::size_t end_pos() const noexcept { assert(operator bool()); return end_position; }
-private:
-    std::size_t position = detail::impl_npos;
-    std::size_t end_position = detail::impl_npos;
+    constexpr explicit search(std::size_t pos, std::size_t end_pos) noexcept : found_pos{pos}, found_end_pos{end_pos} {}
+    constexpr explicit operator bool() const noexcept { return found_pos != detail::impl_npos; }
+    constexpr void reset() noexcept { found_pos = detail::impl_npos; found_end_pos = detail::impl_npos; }
+    constexpr std::size_t pos() const noexcept { assert(operator bool()); return found_pos; }
+    constexpr std::size_t end_pos() const noexcept { assert(operator bool()); return found_end_pos; }
 };
 
 } // namespace una
