424cb5c984416dc2bc08e6e2100790640dac99d5
mg152 <mg152git@gmail.com>
2023-03-30 09:03:19 +0300

    Rename search class to found
    This commit doesn't compile and a part of the following commit.

--- include/uni_algo/internal/found.h
+++ include/uni_algo/internal/found.h
@@ -2,8 +2,8 @@
  * License: Public Domain or MIT - choose whatever you want.
  * See LICENSE.md */
 
-#ifndef UNI_ALGO_INTERNAL_SEARCH_H_UAIH
-#define UNI_ALGO_INTERNAL_SEARCH_H_UAIH
+#ifndef UNI_ALGO_INTERNAL_FOUND_H_UAIH
+#define UNI_ALGO_INTERNAL_FOUND_H_UAIH
 
 #include <cassert>
 
@@ -11,20 +11,20 @@
 
 namespace una {
 
-class search
+class found
 {
 private:
     std::size_t found_pos = detail::impl_npos;
-    std::size_t found_end_pos = detail::impl_npos;
+    std::size_t found_end = detail::impl_npos;
 public:
-    constexpr search() noexcept = default;
-    constexpr explicit search(std::size_t pos, std::size_t end_pos) noexcept : found_pos{pos}, found_end_pos{end_pos} {}
+    constexpr found() noexcept = default;
+    constexpr explicit found(std::size_t pos, std::size_t end_pos) noexcept : found_pos{pos}, found_end{end_pos} {}
     constexpr explicit operator bool() const noexcept { return found_pos != detail::impl_npos; }
-    constexpr void reset() noexcept { found_pos = detail::impl_npos; found_end_pos = detail::impl_npos; }
+    constexpr void reset() noexcept { found_pos = detail::impl_npos; found_end = detail::impl_npos; }
     constexpr std::size_t pos() const noexcept { assert(operator bool()); return found_pos; }
-    constexpr std::size_t end_pos() const noexcept { assert(operator bool()); return found_end_pos; }
+    constexpr std::size_t end_pos() const noexcept { assert(operator bool()); return found_end; }
 };
 
 } // namespace una
 
-#endif // UNI_ALGO_INTERNAL_SEARCH_H_UAIH
+#endif // UNI_ALGO_INTERNAL_FOUND_H_UAIH
